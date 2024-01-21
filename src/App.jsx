413cd7c94bf773230df9a48e732c1a0bd72cfdf6
Panuwat <netpopo1150@outlook.com>
2024-01-21 16:54:25 +0700

    add Component styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import "./App.css";
+import Button from "./components/Button";
+import Alert from "./components/Alert";
 
 function App() {
   return (
     <div className="App">
       <div className="button-components-section">
         {/* Render ตัว Button 2 แบบ */}
+        <Button type={"primary"} />
+        <Button type={"secondary"} />
       </div>
       <hr />
       <div className="alert-components-section">
+        <Alert status="error" />
+        <br />
+        <Alert status="warning" />
+        <br />
+        <Alert status="info" />
+        <br />
+        <Alert status="success" />
         {/* Render ตัว Alert 4 แบบ */}
       </div>
     </div>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,50 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Button() {}
+export default function Button(props) {
+  if (props.type === "primary") {
+    return (
+      <div>
+        <button
+          css={css`
+            display: block;
+            background-color: #074ee8;
+            color: white;
+            border: none;
+            border-radius: 4px;
+            padding: 0px 16px;
+            height: 50px;
+            width: 170px;
+            font-size: 16px;
+            margin:10px;
+          `}
+        >
+          Button
+        </button>
+      </div>
+    );
+  } else if (props.type === "secondary") {
+    return (
+      <div>
+        <button
+          css={css`
+            display: block;
+            background-color: #07A4E8;
+            color: white;
+            border: none;
+            border-radius: 4px;
+            padding: 0px 16px;
+            height: 50px;
+            width: 170px;
+            font-size: 16px;
+            margin:10px;
+
+          `}
+        >
+          Button
+        </button>
+      </div>
+    );
+  }
+}
